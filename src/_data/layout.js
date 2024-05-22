262554f578447f871896bf505241e3505a1e3561
nriquedev <nrique.dev@gmail.com>
2024-05-22 18:06:19 +0200

    Extended base template for pages

--- src/_data/layout.js
+++ src/_data/layout.js
@@ -1,2 +1,4 @@
 // sets a global layout for all templates, can be overwritten later in the Eleventy Data Cascade
-module.exports = 'base.njk';
+// module.exports = 'base.njk';
+
+module.exports = 'page.njk';
